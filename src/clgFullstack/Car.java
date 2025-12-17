e784c6c820a0aff452eefd7d948fcfeb72c4e7a2
TanviWadhai <tanviwadhai@gmail.com>
2025-12-17 13:34:21 +0530

    changes made in car class

--- src/clgFullstack/Car.java
+++ src/clgFullstack/Car.java
@@ -7,6 +7,7 @@ public class Car {
 	
 	void run() {
 		System.out.println("Car is running.");
+		System.out.println("This is git demo for push");
 	}
 	void stop() {
 		System.out.println("car is Stopping.");
