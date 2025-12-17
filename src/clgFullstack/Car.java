e441e327d5da74526d0ea5b885edbd6691ee8ac0
TanviWadhai <tanviwadhai@gmail.com>
2025-12-17 12:36:46 +0530

    initial commit

---
+++ src/clgFullstack/Car.java
@@ -0,0 +1,14 @@
+package clgFullstack;
+
+public class Car {
+	String color="black";
+	String brand="Tesla";
+	String fuel="Petrol";
+	
+	void run() {
+		System.out.println("Car is running.");
+	}
+	void stop() {
+		System.out.println("car is Stopping.");
+	}
+}
---
+++ src/clgFullstack/Robot.java
@@ -0,0 +1,18 @@
+package clgFullstack;
+
+public class Robot {
+	
+	String Brand="Fanuc";
+	String os="Linux";
+	int microprocessor=8081;
+	
+	void Fight() {
+		System.out.println("Robot is fighting.");
+	}
+	void walk() {
+		System.out.println("Robot is walking.");
+	}
+
+}
+
+
