08f34710495c423f3428e93b64a3827cf8709fa7
TanviWadhai <tanviwadhai@gmail.com>
2025-12-17 16:28:30 +0530

    check octal,hexadecimal,binary, decimal numbers.

--- src/clgFullstack/capacityCheck.java
+++ src/clgFullstack/capacityCheck.java
@@ -5,7 +5,15 @@ public class capacityCheck {
 	public static void main(String[] args) {
 		short a=-129;
 		System.out.println(a);
+		int j=45;
+		System.out.println(j);
+		int b=045;
+		System.out.println(b);
+		int c=0x45;
+		System.out.println(c);
+		int d=0b1011;
+		System.out.println(d);
 
-	}
+	} 
 
 }
